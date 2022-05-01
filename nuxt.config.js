a002916b04e08cd9e9c1f0f4249f22b4bc4f9b89
redfox-mx <hernandezdj98@outlook.com>
2022-05-01 15:08:46 -0500

    build(assets): configure sass loader
    - create scss  base files
    - implements google fonts auto attach

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/scss/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -27,11 +27,29 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
+    '@nuxtjs/google-fonts',
   ],
+
+  googleFonts: {
+    display: 'swap',
+    families: {
+      'Open+Sans': {
+        wght: [400, 600, 700, 800],
+      },
+    },
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    loaders: {
+      scss: {
+        sassOptions: {
+          includePaths: ['assets/scss'],
+        },
+      },
+    },
+  },
 }
